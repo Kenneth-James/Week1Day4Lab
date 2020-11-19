b02fb4f0217364aeb545c4bb0ebdd72702018393
Kenneth James <kennyjr612@gmail.com>
2020-11-19 08:54:21 -0500

    Day 4 Lab and Exercise

---
+++ lab.js
@@ -0,0 +1,23 @@
+//Exercise 1
+
+let phrase = "Hello, ";
+const myName = "Ken";
+
+console.log(phrase + myName + '!');
+
+
+
+/*
+Find the average of ( 4, 10, 22, -30, 55); Print in console
+Find the average of ( 68, 7, -22, 9, 100); Print in console
+*/
+
+//EXAMPLE 1
+let sum = 4 + 10 + 22 + -30 + 55;
+
+console.log(sum / 5);
+
+//EXAMPLE 2
+let secondSum = 68 + 7 + -22 + 9 + 100;
+
+console.log(secondSum / 5);
